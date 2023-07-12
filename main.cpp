7677aa0956224bba32b67084c3d5b6cfba0b53eb
chendelian <dlchen@i-con.cn>
2023-07-12 10:05:49 +0800

    socket netlink query example

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+
+#include "nlquery.h"
+
+void outputHelp()
+{
+    std::cout<<"-i : enter your ethernet port name"<<std::endl;
+    std::cout<<"-h : print help"<<std::endl;
+}
+
+int main(int argc, char **argv)
+{
+    int opt;
+    netlink nl;
+    int link_status = -1;
+    char dev[16];
+    bool dev_f = false;
+
+    strncpy(dev, "eth0", 16);
+
+    while((opt = getopt(argc, argv, "i:h")) != -1)
+    {
+        switch(opt)
+        {
+            case 'i':
+                strncpy(dev, optarg, 16);
+                dev_f = true;
+                break;
+            case 'h':
+            default:
+                outputHelp();
+                return 0;
+        }
+    }
+
+    if(!dev_f)
+        return 0;
+
+    link_status = nl.query(dev);
+    std::cout<<link_status<<std::endl;
+
+    return 0;
+}
